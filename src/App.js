a0ced86cc971dc1b10d4a3dd1c2c1f31855c1c29
Aradhya kumar <aradhyasaumay21@gmail.com>
2022-10-29 15:23:34 +0530

    navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/navbar";
+import Sidebar from "./components/Sidebar"
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./sidebar.css";
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+        <li>Profile</li>
+        </div>
+    )
+}
+
+export default Sidebar
