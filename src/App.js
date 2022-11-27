2e21b58e396f793e5b81f82b08fe84ce1377a190
Aradhya kumar <aradhyasaumay21@gmail.com>
2022-11-27 23:23:18 +0530

    stocks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from "react";
 import Navbar from "./components/navbar";
 import Box from "./components/Box"
+import Newsslider from "./components/Newsslider";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Box />
+      <Newsslider />
     </div>
   );
 }
